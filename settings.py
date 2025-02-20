80cd668d9398dcaff0ff23d9bb9de770417ce460
Madyno54 <99693689+Madyno54@users.noreply.github.com>
2025-02-20 01:20:16 +0000

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,13 @@
+import os
+import dj_database_url
+
+DATABASES = {
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600
+    )
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
