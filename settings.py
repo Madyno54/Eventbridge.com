a697ca4168e276b1ab0bc86c9ca5cbb27b330839
Madyno54 <99693689+Madyno54@users.noreply.github.com>
2025-02-20 00:53:19 +0000

    Update settings.py

--- settings.py
+++ settings.py
@@ -11,7 +11,3 @@ DATABASES = {
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-MIDDLEWARE = [
-    'whitenoise.middleware.WhiteNoiseMiddleware',
-    # ...
-]
